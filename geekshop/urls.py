1c0542d8c7eefb2754c0501558c85e9c3e7d4294
yalovoyruslan <78997004+yalovoyruslan@users.noreply.github.com>
2022-01-23 20:27:45 +0200

    Lesson 2 (#2)
    * Lesson_2. Templetes
    
    * Lesson 2. Context for templates
    
    * Lesson 2. Repeat and filters

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -1,27 +1,16 @@
-"""geekshop URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
 from django.contrib import admin
 from django.urls import path
 
 import mainapp.views as mainapp
 
-urlpatterns =[
+urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", mainapp.main),
-    path("products/", mainapp.products),
-    path("contact/", mainapp.contact),
+    path("", mainapp.main, name="main"),
+    path("products/", mainapp.products, name="products"),
+    path("products/all", mainapp.products, name="products_all"),
+    path("products/home", mainapp.products, name="products_home"),
+    path("products/office", mainapp.products, name="products_office"),
+    path("products/modern", mainapp.products, name="products_modern"),
+    path("products/classic", mainapp.products, name="products_classic"),
+    path("contact/", mainapp.contact, name="contact"),
 ]
